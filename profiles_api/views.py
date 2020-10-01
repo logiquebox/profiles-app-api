e49abc8b7d9232e9688fadd7420b7e91d14b9272
Ujah Emmanuel <ujahemmanuel72@gmail.com>
2020-10-01 10:36:28 +0100

    Added a POST method to HelloApiView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,14 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializers
+
 
 class HelloApiView(APIView):
   """Test Api View"""
+  serializer_class = serializers.HelloSerializer
+
 
   def get(self, request, format=None):
     """Returns a list of APIView features"""
@@ -19,3 +25,15 @@ class HelloApiView(APIView):
     }
 
     return Response(context)
+  
+  def post(self, request):
+    """creates a hello message with our name"""
+    serializer = self.serializer_class(data=request.data)
+
+    if serializer.is_valid():
+      name = serializer.validated_data.get('name')
+      message = f'Hello {name} '
+
+      return Response({'message': message}) 
+    else:
+      return Response(serializer.errors, status=status.HTTP_404_BAD_REQUEST)
